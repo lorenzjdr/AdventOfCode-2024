213701df2534eb746993bab961687f3a3cc6061f
lorenzjdr <lorenz.jdr@gmail.com>
2024-12-02 17:53:33 -0600

    Added Day 3 Template

---
+++ Day3/main.py
@@ -0,0 +1,13 @@
+class DayThree:
+   def __init__(self):
+      pass
+
+   def read_input(self):
+      with open('./Day3/input.txt', 'r') as file:
+         pass
+   
+   def Solution(self):
+      pass
+   
+   def SolutionPartTwo(self):
+      pass
